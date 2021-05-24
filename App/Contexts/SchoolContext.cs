c6d726e1884cb3221c0d79de1d9f4cf199d442e5
GannonDeWalt <gannondew@gmail.com>
2021-05-24 13:56:12 -0500

    Added more to DB

--- App/Contexts/SchoolContext.cs
+++ App/Contexts/SchoolContext.cs
@@ -24,12 +24,16 @@ namespace App {
             new Student { Id = 1, FirstName = "Pickle", LastName = "Rick" },
             new Student { Id = 2, FirstName = "Rick", LastName = "Sanchez" },
             new Student { Id = 3, FirstName = "Tom", LastName = "Hanks" },
+            new Student { Id = 4, FirstName = "Sussy", LastName = "Backa" },
             });
 
             modelBuilder.Entity<Grade>().HasData(new Grade[] { 
-            new Grade { Id = 1, StudentId = 1, GradeP = 0.20F },
-            new Grade { Id = 2, StudentId = 2, GradeP = 0.90F },
-            new Grade { Id = 3, StudentId = 3, GradeP = 0.25F },
+            new Grade { Id = 1, StudentId = 1, CourseName= "Spanish", GradeP = 0.20F },
+            new Grade { Id = 2, StudentId = 2, CourseName = "Geometry", GradeP = 0.90F },
+            new Grade { Id = 3, StudentId = 3, CourseName= "Mathematics", GradeP = 0.25F },
+            new Grade {Id = 4, StudentId = 1, CourseName= "Mathematics", GradeP = 0.75F },
+            new Grade {Id = 5, StudentId = 2, CourseName= "Mathematics", GradeP = 0.85F },
+            new Grade {Id = 6, StudentId = 3, CourseName= "Geometry", GradeP = 0.75F }
             });
 
             base.OnModelCreating(modelBuilder);
---
+++ App/Migrations/20210524185446_MoreInfointoDB.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class MoreInfointoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CourseName",
+                value: "Spanish");
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CourseName",
+                value: "Geometry");
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CourseName",
+                value: "Mathematics");
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "GradeP", "StudentId" },
+                values: new object[] { 4, "Mathematics", 0.75f, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "GradeP", "StudentId" },
+                values: new object[] { 5, "Mathematics", 0.85f, 2 });
+
+            migrationBuilder.InsertData(
+                table: "Grades",
+                columns: new[] { "Id", "CourseName", "GradeP", "StudentId" },
+                values: new object[] { 6, "Geometry", 0.75f, 3 });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "FirstName", "LastName", "classification" },
+                values: new object[] { 4, 0, "Sussy", "Backa", 0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CourseName",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CourseName",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Grades",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CourseName",
+                value: null);
+        }
+    }
+}
